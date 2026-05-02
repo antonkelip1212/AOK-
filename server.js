880ee1e6accee746b67c7061f704f96dcf05e45b
AOK Admin <admin@aok.local>
2026-05-02 07:27:16 +0300

    Bind server to 0.0.0.0

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
